968a9f802ce4f1d120f33adf458c90227fcddcc5
Youssef Menyouby <ymenyoub@e2r11p1.1337.ma>
2023-06-09 22:47:23 +0100

    push

--- philo/init.c
+++ philo/init.c
@@ -6,11 +6,33 @@
 /*   By: ymenyoub <ymenyoub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 21:59:21 by ymenyoub          #+#    #+#             */
-/*   Updated: 2023/06/09 04:21:51 by ymenyoub         ###   ########.fr       */
+/*   Updated: 2023/06/09 22:26:41 by ymenyoub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+void	norm(t_rules *philo, pthread_mutex_t *right,
+	pthread_mutex_t *left, long id)
+{
+	pthread_mutex_lock(right);
+	print(philo, "has taken a fork\n", current_time(philo), id);
+	pthread_mutex_lock(left);
+	print(philo, "has taken a fork\n", current_time(philo), id);
+	pthread_mutex_lock(&philo->mutex);
+	philo->last_eat[id] = current_time(philo);
+	pthread_mutex_unlock(&philo->mutex);
+	print(philo, "is eating\n", current_time(philo), id);
+	sleep_time(philo->t_to_eat);
+	pthread_mutex_lock(&philo->mutex);
+	philo->meals++;
+	pthread_mutex_unlock(&philo->mutex);
+	pthread_mutex_unlock(right);
+	pthread_mutex_unlock(left);
+	sleep_time(philo->t_to_sleep);
+	print(philo, "is sleeeping\n", current_time(philo), id);
+	print(philo, "is thinking\n", current_time(philo), id);
+}
 
 void	*routine(void *arg)
 {
@@ -26,46 +48,38 @@ void	*routine(void *arg)
 	id = (long)philo->id;
 	pthread_mutex_unlock(&philo->mutex);
 	while (1)
+		norm(philo, right, left, id);
+}
+
+int	check_nb_must_eat(t_rules *philo)
+{
+	int	j;
+
+	pthread_mutex_lock(&philo->mutex);
+	if (philo->nb_must_eat != -1
+		&& philo->nb_must_eat * philo->nb_philo == philo->meals)
 	{
-		pthread_mutex_lock(right);
-		print(philo, "has taken a fork\n", current_time(philo), id);
-		pthread_mutex_lock(left);
-		print(philo, "has taken a fork\n", current_time(philo), id);
-		pthread_mutex_lock(&philo->mutex);
-		philo->last_eat[id] = current_time(philo);
-		pthread_mutex_unlock(&philo->mutex);
-		print(philo, "is eating\n", current_time(philo), id);
-		sleep_time(philo->t_to_eat);
-		pthread_mutex_lock(&philo->mutex);
-		philo->meals++;
-		pthread_mutex_unlock(&philo->mutex);
-		pthread_mutex_unlock(right);
-		pthread_mutex_unlock(left);
-		sleep_time(philo->t_to_sleep);
-		print(philo, "is sleeeping\n", current_time(philo), id);
-		print(philo, "is thinking\n", current_time(philo), id);
+		j = 0;
+		while (j < philo->nb_philo)
+		{
+			pthread_detach(philo->philo[j]);
+			j++;
+		}
+		return (1);
 	}
+	pthread_mutex_unlock(&philo->mutex);
+	return (0);
 }
 
 int	check_death(t_rules *philo)
 {
 	int	i;
-	int	j;
 
 	while (1)
 	{
 		i = 0;
-		if (philo->nb_must_eat != -1
-			&& philo->nb_must_eat * philo->nb_philo == philo->meals)
-		{
-			j = 0;
-			while (j < philo->nb_philo)
-			{
-				pthread_detach(philo->philo[j]);
-				j++;
-			}
+		if (check_nb_must_eat(philo))
 			return (1);
-		}
 		pthread_mutex_lock(&philo->mutex);
 		while (i < philo->nb_philo)
 		{
@@ -87,7 +101,6 @@ int	init_rules(char **av, t_rules *rules)
 	rules->nb_philo = (ft_atoi(av[1]));
 	if (rules->nb_philo > 200 || rules->nb_philo < 1)
 		return (printf("More 200 philosofers or None\n"));
-	rules->last_eat = malloc(rules->nb_philo * sizeof(long));
 	rules->t_to_die = (ft_atoi(av[2]));
 	rules->t_to_eat = (ft_atoi(av[3]));
 	rules->t_to_sleep = (ft_atoi(av[4]));
@@ -100,48 +113,7 @@ int	init_rules(char **av, t_rules *rules)
 	rules->forks = malloc(sizeof(pthread_mutex_t) * rules->nb_philo);
 	rules->philo = malloc(rules->nb_philo * sizeof(pthread_t));
 	rules->t_current = ft_time();
-	rules->meals = 0;
 	if (!rules->forks)
 		return (1);
 	return (0);
 }
-
-// int init_philo(t_rules *rules)
-// {
-// 	int i;
-// 	i = 0;
-// 	pthread_mutex_init(&rules->mutex, NULL);
-// 	pthread_mutex_init(&rules->print, NULL);
-// 	while (i < rules->nb_philo)
-// 	{
-// 		pthread_mutex_init(&rules->forks[i], NULL);
-// 		i++;
-// 	}
-// 	i = 0;
-// 	while (i < rules->nb_philo)
-// 	{
-// 		pthread_mutex_lock(&rules->mutex);
-// 		rules->id = i;
-// 		rules->last_eat[i] = current_time(rules);
-// 		pthread_mutex_unlock(&rules->mutex);
-// 		if (pthread_create(&rules->philo[i], NULL, &routine, rules))
-// 		{
-// 			printf("Error\n, Creation failed!!");
-// 			return (1);
-// 		}
-// 		usleep(100);
-// 		i++;
-// 	}
-// 	return (0);
-// 	// i  = 0;
-// 	// while (i < rules->nb_philo)
-// 	// {
-// 	// 	if (pthread_join(rules->philo[i], NULL) != 0)
-// 	// 	{
-// 	// 		printf("Error\n, Creation failed!!");
-// 	// 		return (1);
-// 	// 	}
-// 	// 	i++;
-// 	// }
-// 	// pthread_mutex_destroy(&rules->mutex);
-// }
--- philo/main.c
+++ philo/main.c
@@ -6,34 +6,39 @@
 /*   By: ymenyoub <ymenyoub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 16:22:42 by ymenyoub          #+#    #+#             */
-/*   Updated: 2023/06/09 04:17:26 by ymenyoub         ###   ########.fr       */
+/*   Updated: 2023/06/09 22:31:23 by ymenyoub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-// int	main(int ac, char **av)
-// {
-// 	(void)av;
-// 	t_rules *rules;
+void	create_thread(t_rules *rules)
+{
+	int	i;
 
-//     rules = malloc(sizeof(t_rules));
-// 	if (ac < 5 || ac > 6)
-// 		return(exit_error());
-// 	if (check_Args(av))
-// 		return 1;
-// 	if (init_rules(av, info))
-// 		return 1;
-// 	if (init_philo(info))
-// 		return 1;
-// 	return (check_death(&info));
-// }
+	i = 0;
+	while (i < rules->nb_philo)
+	{
+		pthread_mutex_lock(&rules->mutex);
+		rules->id = i;
+		rules->last_eat[i] = current_time(rules);
+		pthread_mutex_unlock(&rules->mutex);
+		if (pthread_create(&rules->philo[i], NULL, &routine, rules))
+		{
+			printf("Error\n, Creation failed!!");
+			return ;
+		}
+		usleep(100);
+		i++;
+	}
+}
+
 int	main(int ac, char **av)
 {
-	(void)av;
 	int		i;
 	t_rules	*rules;
 
+	(void)av;
 	rules = malloc(sizeof(t_rules));
 	if (ac < 5 || ac > 6)
 		return (exit_error());
@@ -49,21 +54,7 @@ int	main(int ac, char **av)
 		pthread_mutex_init(&rules->forks[i], NULL);
 		i++;
 	}
-	i = 0;
 	rules->last_eat = malloc(sizeof(long) * rules->nb_philo);
-	while (i < rules->nb_philo)
-	{
-		pthread_mutex_lock(&rules->mutex);
-		rules->id = i;
-		rules->last_eat[i] = current_time(rules);
-		pthread_mutex_unlock(&rules->mutex);
-		if (pthread_create(&rules->philo[i], NULL, &routine, rules))
-		{
-			printf("Error\n, Creation failed!!");
-			return (1);
-		}
-		usleep(100);
-		i++;
-	}
+	create_thread(rules);
 	return (check_death(rules));
 }
